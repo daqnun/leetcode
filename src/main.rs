e132af23c3819a31ec15fd41f6c4d160e735e36a
daqnun <daqnun@gmail.com>
2025-12-01 08:29:51 -0400

    feat: problem 3190

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[path = "1920.build-array-from-permutation.rs"]
 mod p1920;
+#[path = "3190.find-minimum-operations-to-make-all-elements-divisible-by-three.rs"]
+mod p3190;
 #[path = "3512.minimum-operations-to-make-array-sum-divisible-by-k.rs"]
 mod p3512;
 
