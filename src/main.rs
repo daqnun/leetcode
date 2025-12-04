39152af1903d526aafee78d70290bf745416991e
daqnun <daqnun@gmail.com>
2025-12-04 19:47:15 -0400

    feat: problem 2011

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[path = "1920.build-array-from-permutation.rs"]
 mod p1920;
+#[path = "1929.concatenation-of-array.rs"]
+mod p1929;
+#[path = "2011.final-value-of-variable-after-performing-operations.rs"]
+mod p2011;
 #[path = "3190.find-minimum-operations-to-make-all-elements-divisible-by-three.rs"]
 mod p3190;
 #[path = "3512.minimum-operations-to-make-array-sum-divisible-by-k.rs"]
 mod p3512;
 #[path = "3668.restore-finishing-order.rs"]
 mod p3668;
-#[path = "1929.concatenation-of-array.rs"]
-mod p1929;
 
 fn main() {
     println!("Hello, LeetCode!");
